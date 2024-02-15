96a600ebed03e3fc76857fb0c8e583ad5ccdb85e
dumptyhumpty2014 <dumptyhumpty2014@gmail.com>
2024-02-15 15:44:52 +0300

    example 4

--- lib/main.dart
+++ lib/main.dart
@@ -72,6 +72,96 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  void example2() {
+    final startTime = DateTime.now().toIso8601String();
+    if (kDebugMode) {
+      print('start example 2 $startTime');
+    }
+    Future.delayed(
+      const Duration(seconds: 1),
+      () {
+        if (kDebugMode) {
+          print('in example 2 ${DateTime.now().toIso8601String()}');
+        }
+        for (var i = 0; i < 100; i++) {}
+      },
+    ).then((value) => print('callback example 2')).catchError(
+      (e) {
+        if (kDebugMode) {
+          print(e);
+        }
+      },
+    );
+    if (kDebugMode) {
+      print('end example 2 $startTime - ${DateTime.now().toIso8601String()}');
+    }
+  }
+
+  void example3() {
+    final startTime = DateTime.now().toIso8601String();
+    if (kDebugMode) {
+      print('start example 3 $startTime');
+    }
+    asyncFunc3();
+    if (kDebugMode) {
+      print('end example 2 $startTime - ${DateTime.now().toIso8601String()}');
+    }
+  }
+
+  Future<void> asyncFunc3() async {
+    final value = await mainFunc3();
+    callBack3(value);
+    //mainFunc3().then(callBack3);
+  }
+
+  Future<bool> mainFunc3() async {
+    if (kDebugMode) {
+      print('in example 3 ${DateTime.now().toIso8601String()}');
+    }
+    for (var i = 0; i < 100; i++) {}
+    return true;
+  }
+
+  void callBack3(bool value) {
+    if (kDebugMode) {
+      print('callback example 3 $value');
+    }
+  }
+
+  String extValue = 'init';
+
+  void example4() {
+    if (kDebugMode) {
+      print('start4 extValue=$extValue');
+    }
+    asyncFunc4();
+    if (kDebugMode) {
+      print('end4 extValue=$extValue');
+    }
+  }
+
+  Future<void> asyncFunc4() async {
+    await mainFunc4();
+    if (kDebugMode) {
+      print('in asyncFunc4 $extValue');
+    }
+    callBack4();
+    if (kDebugMode) {
+      print('after callBack4 $extValue');
+    }
+  }
+
+  Future<void> mainFunc4() async {
+    extValue = 'in mainFunc4';
+    Future(() {
+      extValue = 'in Future 4';
+    });
+  }
+
+  void callBack4() {
+    extValue = 'in callBack4';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -88,6 +178,8 @@ class _MyHomePageState extends State<MyHomePage> {
             children: <Widget>[
               ElevatedButton(onPressed: example1, child: const Text('Example 1')),
               ElevatedButton(onPressed: nuance1, child: const Text('Nuance 1')),
+              ElevatedButton(onPressed: example3, child: const Text('Example 3')),
+              ElevatedButton(onPressed: example4, child: const Text('Example 4')),
             ],
           ),
         ),
